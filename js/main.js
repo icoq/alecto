6d9b5233549d3d284d154f682796e31d870e1f64
Hike <hakosargsian@gmail.com>
2020-06-18 12:53:40 +0400

    d

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const modal = document.querySelector(".nav>.navCont");
 const items = document.querySelectorAll(".navItems__item-inner");
 const burger = document.querySelector(".burger");
 const burgerFix = document.querySelector(".burger>span");
+const navSearch = document.querySelector(".fa-search");
 
 const body = document.querySelector("body");
 
@@ -16,7 +17,9 @@ const navItems = document.querySelectorAll(".navItems__item-inner");
 
 window.onclick = function (event) {
   for (const navItem of navItems) {
-    if ($(".navCont").hasClass("active") && event.target == navItem) {
+    if ($(".navCont").hasClass("active") && event.target == navSearch) {
+      continue;
+    } else if ($(".navCont").hasClass("active") && event.target == navItem) {
       $(".navCont").removeClass("active");
       $(".burger").removeClass("active");
       $("body").removeClass("scrollLock");
@@ -73,49 +76,6 @@ window.onload = function () {
   })
     .setTween(timeline2)
     .addTo(controller);
-
-  $('a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function (event) {
-      // On-page links
-      if (
-        location.pathname.replace(/^\//, "") ==
-          this.pathname.replace(/^\//, "") &&
-        location.hostname == this.hostname
-      ) {
-        // Figure out element to scroll to
-        var target = $(this.hash);
-        target = target.length
-          ? target
-          : $("[name=" + this.hash.slice(1) + "]");
-        // Does a scroll target exist?
-        if (target.length) {
-          // Only prevent default if animation is actually gonna happen
-          event.preventDefault();
-          $("html, body").animate(
-            {
-              scrollTop: target.offset().top,
-            },
-            1000,
-            function () {
-              // Callback after animation
-              // Must change focus!
-              var $target = $(target);
-              $target.focus();
-              if ($target.is(":focus")) {
-                // Checking if the target was focused
-                return false;
-              } else {
-                $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
-                $target.focus(); // Set focus again
-              }
-            }
-          );
-        }
-      }
-    });
 
   $(window).on("mousemove", function (e) {
     let w = $(window).width();
